7e1bba5c696d09bb65c65fb2e04f5caf3d2d5499
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-08-15 11:59:53 -0700

    Export relevant classes to root namespace

---
+++ singlet/__init__.py
@@ -0,0 +1,8 @@
+# vim: fdm=indent
+# author:     Fabio Zanini
+# date:       15/08/17
+# content:    Main singlet module.
+# Module exporting
+from .samplesheet import SampleSheet
+from .counts_table import CountsTable
+from .dataset import Dataset
