b83b2fa2ca4792db7068962c5219f9506feadf07
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-08-15 15:54:32 -0700

    Normalization of CountsTable

--- singlet/counts_table.py
+++ singlet/counts_table.py
@@ -14,7 +14,7 @@ class CountsTable(pd.DataFrame):
     - Columns are samples.
     '''
 
-    _metadata = ['name', '_spikeins', '_otherfeatures']
+    _metadata = ['name', '_spikeins', '_otherfeatures', '_normalized']
 
     @property
     def _constructor(self):
@@ -37,6 +37,7 @@ class CountsTable(pd.DataFrame):
         self.name = tablename
         self._spikeins = config['io']['count_tables'][tablename]['spikeins']
         self._otherfeatures = config['io']['count_tables'][tablename]['other']
+        self._normalized = config['io']['count_tables'][tablename]['normalized']
 
         return self
 
@@ -72,3 +73,48 @@ class CountsTable(pd.DataFrame):
             CountsTable: a slice of self with only other features (e.g. unmapped).
         '''
         return self.loc[self._otherfeatures]
+
+    def normalize(self, method='counts_per_million', include_spikeins=False, inplace=False, **kwargs):
+        '''Normalize counts and return new CountsTable.
+
+        Args:
+            method (string or function): The method to use for normalization. One of
+            'counts_per_million', 'counts_per_thousand_spikeins',
+            'counts_per_thousand_features'. If this argument is a function, it
+            must take the CountsTable as input and return the normalized one as output.
+            include_spikeins (bool): Whether to include spike-ins in the
+            normalization and result.
+            inplace (bool): Whether to modify the CountsTable in place or return
+            a new one.
+
+        Returns:
+            If `inplace` is False, a new, normalized CountsTable.
+        '''
+        if self._normalized:
+            raise ValueError('CountsTable is already normalized')
+
+        if method == 'counts_per_million':
+            counts = self.exclude_features(spikeins=(not include_spikeins), other=True)
+            counts_norm = 1e6 * counts / counts.sum(axis=0)
+        elif method == 'counts_per_thousand_spikeins':
+            counts = self.exclude_features(spikeins=(not include_spikeins), other=True)
+            norm = self.get_spikeins().sum(axis=0)
+            counts_norm = 1e3 * counts / norm
+        elif method == 'counts_per_thousand_features':
+            if 'features' not in kwargs:
+                raise ValueError('Set features=<list of normalization features>')
+            counts = self.exclude_features(spikeins=(not include_spikeins), other=True)
+            norm = self.loc[kwargs['features']].sum(axis=0)
+            counts_norm = 1e3 * counts / norm
+        elif callable(method):
+            counts_norm = method(self)
+            method = 'custom'
+        else:
+            raise ValueError('Method not understood')
+
+        if inplace:
+            self.loc[:, :] = counts_norm.values
+            self._normalized = method
+        else:
+            counts_norm._normalized = method
+            return counts_norm
