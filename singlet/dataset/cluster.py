247520ddfa4e45f9fe5a617eae76c223e36f1625
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-10-16 14:40:34 -0700

    DBSCAN clustering

--- singlet/dataset/cluster.py
+++ singlet/dataset/cluster.py
@@ -68,6 +68,52 @@ class Cluster():
         labels = pd.Series(kmeans.labels_, index=data.index)
         return labels
 
+    def dbscan(
+            self,
+            axis,
+            phenotypes=(),
+            log_features=True,
+            **kwargs,
+            ):
+        '''Density-Based Spatial Clustering of Applications with Noise.
+
+        Args:
+            axis (string): It must be 'samples' or 'features'. \
+                    The Dataset.counts matrix is used and \
+                    either samples or features are clustered.
+            phenotypes (iterable of strings): Phenotypes to add to the \
+                    features for joint clustering.
+            log_features (bool): Whether to add pseudocounts and take a log \
+                    of the feature counts before calculating distances.
+            **kwargs: arguments passed to sklearn.cluster.DBSCAN.
+
+        Returns:
+            pd.Series with the labels of the clusters.
+        '''
+        from sklearn.cluster import DBSCAN
+
+        data = self.dataset.counts
+
+        if log_features:
+            data = np.log10(self.dataset.counts.pseudocount + data)
+
+        if phenotypes is not None:
+            data = data.copy()
+            for pheno in phenotypes:
+                data.loc[pheno] = self.dataset.samplesheet.loc[:, pheno]
+
+        if axis == 'samples':
+            data = data.T
+        elif axis == 'features':
+            pass
+        else:
+            raise ValueError('axis must be "samples" or "features"')
+
+        kmeans = (DBSCAN(**kwargs)
+                  .fit(data.values))
+        labels = pd.Series(kmeans.labels_, index=data.index)
+        return labels
+
     # NOTE: caching this one is tricky because it has non-kwargs AND it would
     # need a double cache, one for cells and one for features/phenotypes
     def hierarchical(
