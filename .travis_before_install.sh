87f63aa86f9d57ac4e1e8591de17777fa943ba9d
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-09-09 22:18:46 -0700

    Install miniconda 3.7 on OSX

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -8,10 +8,10 @@ elif [ $TRAVIS_OS_NAME == 'osx' ]; then
   echo "Installing deps for OSX"
   if [ $PYTHON_VERSION == "2.7" ]; then
     CONDA_VER='2'
-  elif [ $PYTHON_VERSION == "3.6" ]; then
+  elif [ $PYTHON_VERSION == "3.7" ]; then
     CONDA_VER='3'
   else
-    echo "Conda only supports 2.7 and 3.6"
+    echo "Miniconda only supports 2.7 and 3.7"
   fi
   curl "https://repo.continuum.io/miniconda/Miniconda${CONDA_VER}-latest-MacOSX-x86_64.sh" -o "miniconda.sh"
   bash "miniconda.sh" -b -p $HOME/miniconda
