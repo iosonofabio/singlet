66c9913bf95f99062615a98e3ac6c51750e9b31f
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-01-30 22:23:08 -0800

    Simplify tests

--- .travis_test.sh
+++ .travis_test.sh
@@ -17,30 +17,4 @@ echo 'Running pytests...'
 # LOCAL TESTING:
 # PYTHONPATH=$(pwd):PYTHONPATH SINGLET_CONFIG_FILENAME='example_data/config_example.yml' pytest -rxXs test
 
-echo 'IO tests...'
-${PYTEST} "test/io"
-if [ $? != 0 ]; then
-    exit 1
-fi
-echo 'done!'
-
-echo 'Samplesheet tests...'
-${PYTEST} "test/samplesheet"
-if [ $? != 0 ]; then
-    exit 1
-fi
-echo 'done!'
-
-echo 'Counts table tests...'
-${PYTEST} "test/counts_table"
-if [ $? != 0 ]; then
-    exit 1
-fi
-echo 'done!'
-
-echo 'Dataset tests...'
-${PYTEST} "test/dataset"
-if [ $? != 0 ]; then
-    exit 1
-fi
-echo 'done!'
+${PYTEST} "test"
