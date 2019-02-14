38badcb03ca1f7a8cbe1997ae94467adac498866
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-02-13 17:51:25 -0800

    Counts table I/O with different bit precisions

--- test/counts_table/test_initialize.py
+++ test/counts_table/test_initialize.py
@@ -6,14 +6,25 @@ date:       15/08/17
 content:    Test CountsTable class.
 '''
 def test_initialize():
-    print('Instantiating CountsTable')
     from singlet.counts_table import CountsTable
     ct = CountsTable.from_tablename('example_table_tsv')
-    print('Done!')
 
 
 def test_initialize_fromdataset():
-    print('Instantiating CountsTable')
     from singlet.counts_table import CountsTable
     ct = CountsTable.from_datasetname('example_dataset')
-    print('Done!')
+
+
+def test_initialize_128():
+    from singlet.counts_table import CountsTable
+    ct = CountsTable.from_tablename('example_table_tsv_float128')
+
+
+def test_initialize_32():
+    from singlet.counts_table import CountsTable
+    ct = CountsTable.from_tablename('example_table_tsv_float32')
+
+
+def test_initialize_16():
+    from singlet.counts_table import CountsTable
+    ct = CountsTable.from_tablename('example_table_tsv_float16')
