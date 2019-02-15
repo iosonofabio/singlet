bfb325c58b3195befadfbf9b623d2f613352f793
Fabio Zanini <fabio.zanini@fastmail.fm>
2019-02-14 16:51:37 -0800

    Clean up some tests

--- test/dataset/test_feature_selection.py
+++ test/dataset/test_feature_selection.py
@@ -22,46 +22,34 @@ def ds():
 
 
 def test_unique(ds):
-    print('Test feature selection by expression')
     res = ds.feature_selection.unique()
     assert('TSPAN6' in res[0])
-    print('Done!')
 
 
 def test_expression(ds):
-    print('Test feature selection by expression')
     res = ds.feature_selection.expressed(n_samples=1, exp_min=1)
     assert(res[0] == 'TSPAN6')
-    print('Done!')
 
 
 def test_expression_inplace(ds):
-    print('Test feature selection by expression, in place')
     dsp = ds.copy()
     dsp.feature_selection.expressed(n_samples=1, exp_min=1, inplace=True)
     assert(dsp.featurenames[0] == 'TSPAN6')
-    print('Done!')
 
 
 def test_overdispersed_strata(ds):
-    print('Test feature selection by overdispersed strata')
     res = ds.feature_selection.overdispersed_strata()
     assert(res[-1] == 'FTL')
-    print('Done!')
 
 
 def test_overdispersed_strata_inplace(ds):
-    print('Test feature selection by overdispersed strata, in place')
     dsp = ds.copy()
     dsp.feature_selection.overdispersed_strata(inplace=True)
     assert(dsp.featurenames[-1] == 'FTL')
-    print('Done!')
 
 
 @pytest.mark.xfail(True, reason='SAM has fragile deps and APIs')
 def test_selfassemblingmanifolds(ds):
-    print('Test feature weight by self assembling manifolds')
     dsp = ds.copy()
     sam = dsp.feature_selection.sam(npcs=3)
     weights = sam.output_vars['gene_weights']
-    print('Done!')
